b12263980fa9ae54389f5c8fb03e3dd7fee585c7
Ajbakaric <bakaricaustin@yahoo.com>
2025-07-26 13:38:08 -0600

    new build command file

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Fail fast on error
+set -e
+
+# Build frontend
+npm install --prefix frontend
+npm run build --prefix frontend
+cp -a frontend/dist/* public/
+
+# Build backend
+bundle install
+bundle exec rake db:migrate
+bundle exec rake db:seed
