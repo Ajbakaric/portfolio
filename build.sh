52f039ada03cfb74d486e6580bdc959327df9098
Ajbakaric <bakaricaustin@yahoo.com>
2025-08-07 13:09:25 -0600

    rails runner added

--- build.sh
+++ build.sh
@@ -12,5 +12,6 @@ bundle install --without development test
 
 # TEMP OK if you can't set a Post-deploy command in Render yet:
 bundle exec rails db:migrate
+bundle exec rails runner 'puts Project.pluck(:title, :link, :repo).inspect'
 
 # IMPORTANT: Do NOT destroy data here.
